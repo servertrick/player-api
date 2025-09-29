6dfbbddf988f7d7b36923104afd9cb9d6807d599
Michael Johnson <michael.johnson3@hpe.com>
2025-09-29 15:50:05 -0500

    Added player code

--- src/App.js
+++ src/App.js
@@ -21,27 +21,6 @@ const Tile = ({ title, content }) => (
   </Card>
 );
 
-import React from 'react';
-import { Grommet, Box } from 'grommet';
-
-const App = () => (
-  <Grommet>
-    <Box align="center" pad="medium">
-      <Box width="large" height="medium" overflow="hidden" round="small" elevation="small">
-        <iframe
-          width="100%"
-          height="100%"
-          src="https://sketchfab.com/models/5d570c0e904a434f9a8dbedca39d89b8/embed?ui_infos=0&ui_watermark=0&ui_help=0&ui_vr=0&ui_settings=0&ui_inspector=0&ui_hint=2&ui_color=01a982&ui_theme=dark&autostart=1&camera=0"
-          title="Player API"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          />
-          </Box>
-          </Box>
-          </Grommet>
-)
-
-
 function App() {
   return (
     <Grommet theme={theme} full>
@@ -50,7 +29,9 @@ function App() {
           <Home />
           <Text size="large" weight="bold">Player API Demo</Text>
         </Header>
-
+        <Box fill align="center" justify="center" pad="large" background="light-2">
+          <VideoEmbed videoId="5d570c0e904a434f9a8dbedca39d89b8" />
+        </Box>
         <Box direction="row" wrap pad="medium" justify="center">
           <Tile title="Tile 1" content="This is the first tile." />
           <Tile title="Tile 2" content="This is the second tile." />
---
+++ src/VideoEmbed.js
@@ -0,0 +1,19 @@
+// VideoEmbed.js
+import React from 'react';
+import { Box } from 'grommet';
+
+const VideoEmbed = ({ videoId }) => (
+  <Box width="large" height="medium" round="small" elevation="medium" overflow="hidden">
+    <iframe
+      width="100%"
+      height="100%"
+      src={`https://sketchfab.com/models/${videoID}/embed?ui_infos=0&ui_watermark=0&ui_help=0&ui_vr=0&ui_settings=0&ui_inspector=0&ui_hint=2&ui_color=01a982&ui_theme=dark&autostart=1&camera=0`}
+      title="DL380 Gen12"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  </Box>
+);
+
+export default VideoEmbed;
