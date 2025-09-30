62261f509ad747b2dc73eb290e2ef8129b892379
Michael Johnson <michael.johnson3@hpe.com>
2025-09-30 16:27:06 -0500

    update

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const customTheme = {
   global: {
     colors: {
       brand: '#292d3a', // primary color
-      background: '#f0f0f0',
-      text: '#f0f0f0',
+      background: '#f7f7f7',
+      text: '#ffffff',
     },
     font: {
       family: 'Arial',
@@ -36,34 +36,39 @@ const Tile = ({ title, content }) => (
   </Card>
 );
 
-
 function App() {
   return (
     <Grommet theme={customTheme} full>
       <Box fill>
         <Header background="brand" pad="medium">
-
+          <Home />
           <Text size="large" weight="bold">Player API Demo</Text>
         </Header>
 
-        <Box
-          wrap
-          margin="large"
-          width="large"
-          height="medium"
-          round="medium"
-          elevation="medium"
-          overflow="hidden"
-          alignSelf="center"
-        >
+          <Box wrap margin="large" width="large" height="medium" round="medium" elevation="medium" overflow="hidden" alignSelf="center">
+
           <div style={{ position: "relative", paddingBottom: "50.25%", height: 0 }}>
-            <SketchfabViewer modelUid="5d570c0e904a434f9a8dbedca39d89b8" />
-          </div>
-        </Box>
+      {/* Responsive iframe */}
+      <iframe
+        src={`https://sketchfab.com/models/5d570c0e904a434f9a8dbedca39d89b8/embed?ui_infos=0&ui_watermark=0&ui_help=0&ui_vr=0&ui_settings=0&ui_inspector=0&ui_hint=2&ui_color=01a982&ui_theme=dark&autostart=1&camera=0`}
+        title="YouTube video"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      ></iframe>
+    </div> 
+        
+          </Box>
       </Box>
     </Grommet>
   );
 }
-
 
 export default App;
